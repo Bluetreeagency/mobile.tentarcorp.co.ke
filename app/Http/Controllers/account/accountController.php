e3875db63ea8583254524966d8030883b35dc78e
Blue Tree Digital Agency <45921997+Bluetreeagency@users.noreply.github.com>
2023-02-11 16:31:32 +0300

    mobile version

---
+++ app/Http/Controllers/account/accountController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\account;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class accountController extends Controller
+{
+   //blank
+   public function blank(){
+      return view('blank');
+   }
+}
---
+++ routes/web.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Auth::routes();
+Route::get('logout', [App\Http\Controllers\Auth\LoginController::class, 'logout']);
+Route::get('/', [App\Http\Controllers\Auth\LoginController::class, 'showLoginForm'])->name('login');
+
+Route::get('blank', [App\Http\Controllers\account\accountController::class, 'blank']);
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
